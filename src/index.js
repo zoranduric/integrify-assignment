54dc0966f5f79172f575a044971797c3b5b27bce
Zoran <zordjuric@gmail.com>
2022-08-30 19:42:21 +0300

    clean up

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import User from "./components/User";
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import User from './components/User';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <header className="">
+      <header className=''>
         <p>Integrify Full-Stack Course React Pre-Assigment</p>
       </header>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to='/'>Home</Link>
       </nav>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/user/:id" element={<User />} />
+        <Route path='/' element={<App />} />
+        <Route path='/user/:id' element={<User />} />
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
