2c8d98aa6bcfce969c01a2e529ee8eae4af19d1e
Zoran <zordjuric@gmail.com>
2022-09-07 20:15:41 +0300

    useContext

--- src/components/User.js
+++ src/components/User.js
@@ -1,69 +1,58 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import UserContext from '../context/UsersContext';
 
 const User = () => {
   let { id } = useParams();
-  const [isLoading, setLoading] = useState(true);
-  const [user, updateUser] = useState([]);
+  const { usersAndLoading } = useContext(UserContext);
+  const { users } = usersAndLoading;
+  const user = users.find((user) => user.id === parseInt(id));
+  const { name, username, email, phone, website } = user;
 
-  useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((response) => {
-        updateUser(response.data);
-        setLoading(false);
-      });
-  }, [id]);
+  return (
+    <div>
+      <div className='className_container'>
+        <div className='userDetails'>
+          <span> </span>
+          <p>
+            <span>Name: </span> {name}
+          </p>
+          <p>
+            <span>Username: </span> {username}
+          </p>
+          <p>
+            <span>Email: </span> {email}
+          </p>
+          <p>
+            <span>Phone:</span> {phone}
+          </p>
+          <p>
+            <span>Company:</span> {user.company.name}
+          </p>
+          <p>
+            <span>Website:</span>
+            <a className='userCard__Website' href={website}>
+              {website}
+            </a>
+          </p>
 
-  if (isLoading) {
-    return <p>loading please wait</p>;
-  } else {
-    return (
-      <div>
-        <div className="className_container">
-          <div className="userDetails">
-            <span> </span>
-            <p>
-              <span>Name: </span> {user.name}
-            </p>
-            <p>
-              <span>Username: </span> {user.username}
-            </p>
-            <p>
-              <span>Email: </span> {user.email}
-            </p>
-            <p>
-              <span>Phone:</span> {user.phone}
-            </p>
-            <p>
-              <span>Company:</span> {user.company.name}
-            </p>
-            <p>
-              <span>Website:</span>
-              <a className="userCard__Website" href={user.website}>
-                {user.website}
-              </a>
-            </p>
+          <p>
+            <span>Street:</span> {user.address.street}
+          </p>
 
-            <p>
-              <span>Street:</span> {user.address.street}
-            </p>
-
-            <p>
-              <span>Street:</span> {user.address.suite}
-            </p>
-            <p>
-              <span>Street:</span> {user.address.city}
-            </p>
-            <p>
-              <span>Street:</span> {user.address.zipcode}
-            </p>
-          </div>
+          <p>
+            <span>Street:</span> {user.address.suite}
+          </p>
+          <p>
+            <span>Street:</span> {user.address.city}
+          </p>
+          <p>
+            <span>Street:</span> {user.address.zipcode}
+          </p>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default User;
